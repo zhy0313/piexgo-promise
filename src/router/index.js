4c9291804ad5f825f1dd6a324e289e03d7cbe54c
mengluoH <185617705@qq.com>
2019-02-13 18:40:54 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(Router);
 const router = new Router({
   mode: 'history', // 切换路径模式，变成history模式
   routes: [
-    {path: '/', name: 'HelloWorld', component: resolve => require(["@/components/HelloWorld"], resolve),}
+    {path: '/', name: 'HelloWorld', component: resolve => require(["@/components/Main"], resolve),}
   ]
 });
 

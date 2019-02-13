9b080f80588b4bf5fed148a6fe3397853f3fcde9
mengluoH <185617705@qq.com>
2019-02-13 18:19:50 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: 'history', // 切换路径模式，变成history模式
+  routes: [
+    {path: '/', name: 'HelloWorld', component: resolve => require(["@/components/HelloWorld"], resolve),}
+  ]
+});
+
+export default router;

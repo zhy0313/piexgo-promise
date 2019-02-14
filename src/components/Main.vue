6320d85ea13b9745c30952bd184dbed83af2e70d
mengluoH <185617705@qq.com>
2019-02-14 11:40:10 +0800

    fix

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
-
+  <div :class="wrapperCls">
+    <h1>hello world</h1>
   </div>
 </template>
+
+<script>
+const prefixCls = "piexGo-promise-main";
+export default {
+  data() {
+    return {
+      prefixCls,
+    }
+  },
+  computed: {
+    wrapperCls() {
+      return `${prefixCls}`
+    }
+  }
+}
+</script>
